b901bd04107772e0390e84e83acc0cf34325784b
Ivan Chaban <ivan.chaban@devico.io>
2018-01-11 17:41:40 +0200

    Fix

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,6 @@ module.exports = {
     'postcss-cssnext': {
       browsers: ['last 10 versions']
     },
-    'postcss-short': {},
-    'postcss-sorting': {},
     'postcss-flexbugs-fixes': {},
     'css-mqpacker': {}
   }
