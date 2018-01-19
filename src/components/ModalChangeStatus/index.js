0b53a4ae0275d069a2f303cc633badba209bd498
Ivan Chaban <ivan.chaban@devico.io>
2018-01-19 13:35:20 +0200

    setFilter

--- src/components/ModalChangeStatus/index.js
+++ src/components/ModalChangeStatus/index.js
@@ -1,27 +1,19 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
+import { goBack } from 'react-router-redux';
 import { changeStatus } from '../../actions/todo';
 
-class ModalChangeStatus extends Component {
+class ModalChangeLabel extends Component {
   constructor(props) {
     super(props);
   }
 
   state = {
-    isVisible: this.props.path === '/change-status',
     label: this.props.todo.status
   };
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      isVisible: nextProps.path === '/change-status'
-    });
-  }
-
   handleHide = () => {
-    let id = this.props.match.params.id;
-    this.props.push(`/${id}`);
+    this.props.goBack();
   };
 
   stopPropagation = (e) => {
@@ -49,49 +41,44 @@ class ModalChangeStatus extends Component {
     const { status } = this.props.todo;
     const { label } = this.state;
 
-    if (this.state.isVisible) {
-      return (
-        <div>
-          <div className="modal-overlay" onClick={ this.handleHide }>
-            <div className="modal" onClick={ this.stopPropagation }>
-              <div className="modal-content">
-                <h4> Do you want change label? </h4>
+    return (
+      <div>
+        <div className="modal-overlay" onClick={ this.handleHide }>
+          <div className="modal" onClick={ this.stopPropagation }>
+            <div className="modal-content">
+              <h4> Do you want change label? </h4>
 
-                <select defaultValue={ status } onChange={ this.changeStatus }>
-                  <option value="new">new</option>
-                  <option value="in progress">in progress</option>
-                  <option value="review">review</option>
-                  <option value="completed">completed</option>
-                </select>
-              </div>
-              <div className="modal-footer">
-                <button
-                  className="modal-action"
-                  onClick={ this.disagree }
-                > Disagree </button>
+              <select defaultValue={ status } onChange={ this.changeStatus }>
+                <option value="new">new</option>
+                <option value="in progress">in progress</option>
+                <option value="review">review</option>
+                <option value="completed">completed</option>
+              </select>
+            </div>
+            <div className="modal-footer">
+              <button
+                className="modal-action"
+                onClick={ this.disagree }
+              > Disagree </button>
 
-                <button
-                  className="modal-action"
-                  onClick={ this.agree }
-                  disabled={ label === status }
-                > Agree </button>
-              </div>
+              <button
+                className="modal-action"
+                onClick={ this.agree }
+                disabled={ label === status }
+              > Agree </button>
             </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(state)
-  let id = 0; // todo
+  let id = ownProps.match.params.id;
   let todo = state.todos.filter((el, index) => index === parseInt(id))[0];
-  return {
-    todo,
-    path: state.router.location.pathname
-  }
+  return { todo }
 };
 
-export default connect(mapStateToProps, { push, changeStatus })(ModalChangeStatus)
+export default connect(mapStateToProps, { goBack, changeStatus })(ModalChangeLabel)
+
