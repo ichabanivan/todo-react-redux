43fd86e33150a50fc8c198fee9fa18e60911fee2
Ivan Chaban <ivan.chaban@devico.io>
2018-01-22 19:39:15 +0200

    Todo

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -2,26 +2,31 @@ import CONSTANTS from '../constants/';
 
 let initialState =  [
   {
+    id: Math.floor(Math.random() * 10000),
     body: '1Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh.',
     status: 'new',
     date: 'Fri Jan 12 2018 11:57:06 GMT+0200 (EET)'
   },
   {
+    id: Math.floor(Math.random() * 10000),
     body: '2Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.',
     status: 'review',
     date: 'Fri Jan 12 2018 11:37:01 GMT+0200 (EET)'
   },
   {
+    id: Math.floor(Math.random() * 10000),
     body: '3Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.',
     status: 'completed',
     date: 'Fri Jan 12 2018 11:56:55 GMT+0200 (EET)'
   },
   {
+    id: Math.floor(Math.random() * 10000),
     body: '4quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.',
     status: 'review',
     date: 'Fri Jan 12 2018 11:56:00 GMT+0200 (EET)'
   },
   {
+    id: Math.floor(Math.random() * 10000),
     body: '5D elit, eget tincidunt nibh pulvinar a. 123',
     status: 'in progress',
     date: 'Fri Jan 18 2018 13:56:00 GMT+0200 (EET)'
