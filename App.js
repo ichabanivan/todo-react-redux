bc5a7564b92126bcc1bb4fa4ebd3c7a65a95d238
Ivan Chaban <ivan.chaban@devico.io>
2018-01-24 19:38:50 +0200

    tmp

--- App.js
+++ App.js
@@ -6,63 +6,55 @@ const router = new Router();
 const MongoClient = require('mongodb').MongoClient;
 const koaBody = require('koa-body');
 
-let todos = [];
-
+let todos; // убрать
+let db;
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
 // Database Name
 const dbName = 'todos';
 
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('todos');
-    // Insert some documents
-    collection.insertMany(todos, function(err, result) {
-    callback(result);
-  });
-};
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    todos = docs;
-    callback(docs);
-  });
-};
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  console.log('Connected successfully to server');
-
-  const db = client.db(dbName);
-
-  insertDocuments(db, function () {
-    findDocuments(db, function () {
-      client.close();
-    })
-  })
-});
 
 
 router.post('/getTodos', async (ctx) => {
+  // Use connect method to connect to the server
+  MongoClient.connect(url,  (err, client) => {
+    console.log('Connected successfully to server');
+
+    db = client.db(dbName);
+
+    // Get the documents collection
+    const collection = db.collection('todos');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+      todos = docs;
+      ctx.body = JSON.stringify(docs);
+      client.close();
+    });
+  });
+  console.log(todos)
+
   ctx.body = JSON.stringify(todos);
+
 });
 
 router.post('/addTodo', koaBody(), async (ctx) => {
   console.log(ctx.request.body) // new todo
+  console.log(ctx.request.body.id)
   ctx.body = true;
 });
 
 router.post('/changeStatus', koaBody(), async (ctx) => {
   console.log(ctx.request.body) // todo with new status
+  console.log(typeof ctx.request.body.id)
+
+  // db.update({_id: new ObjectID(id)}, {$set: body}, done);
   ctx.body = true;
 });
 
 router.post('/updateTodo', koaBody(), async (ctx) => {
-  console.log(ctx.request.body) // todo with new status
+  console.log(ctx.request.body) // new todo
   ctx.body = true;
 });
 
